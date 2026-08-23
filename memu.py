e3a8d2602b62d8014e8d638b45146a3bb0a38106
pagadalapranathi-hub <pagadalapranathi48@gmail.com>
2026-08-23 12:17:55 +0530

    Add display_menu function for student management

---
+++ memu.py
@@ -0,0 +1,21 @@
+def display_menu():
+
+    print("\n")
+    print("="*60)
+    print("      STUDENT MANAGEMENT SYSTEM")
+    print("="*60)
+
+    print("1. Register Student")
+    print("2. View Students")
+    print("3. Search Student")
+    print("4. Update Student")
+    print("5. Delete Student")
+    print("6. Dashboard")
+    print("7. Report Card")
+    print("8. Attendance")
+    print("9. CGPA")
+    print("10. Analytics")
+    print("11. Export CSV")
+    print("12. Exit")
+
+    print("="*60)
